1098b619f9a125b46e9bfd92432cae4c70ef84ff
anjalipatelfablead <anjalipatel.fablead@gmail.com>
2026-02-27 10:54:34 +0530

    added main toast

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import AppRoutes from './routes/app_route';
 import Navbar from './components/navbar';
 import Sidebar from './components/sidebar';
 import './App.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -15,6 +17,16 @@ function App() {
       <Navbar onMenuClick={openSidebar} />
       <Sidebar isOpen={sidebarOpen} onClose={closeSidebar} />
       <AppRoutes />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
     </div>
   );
 }

6fc22e2eaff6f1c314282c60d4c6a81eb3aad672
anjalipatelfablead <anjalipatel.fablead@gmail.com>
2026-03-02 14:55:02 +0530

    udated   Quiz list text

--- src/pages/question/QuestionReview.jsx
+++ src/pages/question/QuestionReview.jsx
@@ -101,7 +101,7 @@ const QuestionReview = () => {
                             onClick={handleBackToQuizzes}
                             className="px-4 py-2 border border-gray-300 text-gray-700 rounded-lg hover:bg-gray-50 transition-colors"
                         >
-                            izzes
+                            Quiz list
                         </button>
                     </div>
                 </div>
